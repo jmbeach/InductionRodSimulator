dff25ccf0cfcec84e7f42e72fa091633a9c72c10
Jared Beach <jaredbeachdesign@gmail.com>
2015-11-26 00:12:26 -0600

    completed current arrows

--- Assets/Rod.cs
+++ Assets/Rod.cs
@@ -50,6 +50,7 @@ public class Rod : MonoBehaviour {
         newPosition.Set(newPosition.x, 0, 0);
         // Assign new position.
         transform.position = newPosition;
+        GenerateCurrentArrows();
     }
 
     private void GenerateCurrentArrows()
@@ -69,10 +70,48 @@ public class Rod : MonoBehaviour {
         var firstArrowPosition = new Vector3(5.12f, -.02f, 3.04f);
         var arrowRotation = Quaternion.Euler(0, 0, 0);
         var arrowWidth = CurrentArrow.GetComponent<Renderer>().bounds.size.x;
+        var rodHeight = GetComponent<Renderer>().bounds.size.z;
         var distanceFromFirstToRod = Vector3.Distance(new Vector3(transform.position.x,0,0), new Vector3(firstArrowPosition.x,0,0));
         var arrowsToMakeWidthWise = distanceFromFirstToRod/arrowWidth;
-        for (int i = 0; i < arrowsToMakeWidthWise; i++)
+        var position = firstArrowPosition;
+        for (var i = 0; i < arrowsToMakeWidthWise; i++)
         {
+            var arrow = (GameObject)Instantiate(CurrentArrow, position,arrowRotation);
+            arrow.SetActive(true);
+            position.Set(position.x-arrowWidth,position.y,position.z);
+            CurrentArrows.Add(arrow);
+        }
+        // made it to bar.
+        // go up the rod
+        arrowRotation = Quaternion.Euler(0, 270, 0);
+        var arrowsToMakeHeightWise = rodHeight/arrowWidth;
+        position.Set(transform.position.x,position.y,position.z-arrowWidth);
+        for (var i = 0; i < arrowsToMakeHeightWise-1; i++)
+        {
+            var arrow = (GameObject)Instantiate(CurrentArrow, position,arrowRotation);
+            arrow.SetActive(true);
+            position.Set(transform.position.x,position.y,position.z-arrowWidth);
+            CurrentArrows.Add(arrow);
+        }
+        // go back left
+        arrowRotation = Quaternion.Euler(0, 180, 0);
+        position.Set(position.x,position.y,position.z+arrowWidth/4);
+        for (var i = 0; i < arrowsToMakeWidthWise; i++)
+        {
+            var arrow = (GameObject) Instantiate(CurrentArrow, position, arrowRotation);
+            arrow.SetActive(true);
+            position.Set(position.x+arrowWidth,position.y,position.z);
+            CurrentArrows.Add(arrow);
+        }
+        // go down
+        arrowRotation = Quaternion.Euler(0,90,0);
+        position.Set(firstArrowPosition.x,position.y,position.z+arrowWidth);
+        for (var i = 0; i < arrowsToMakeHeightWise - 1; i++)
+        {
+            var arrow = (GameObject)Instantiate(CurrentArrow, position, arrowRotation);
+            arrow.SetActive(true);
+            position.Set(firstArrowPosition.x, position.y, position.z + arrowWidth);
+            CurrentArrows.Add(arrow);
         }
     }
     private void DeleteCurrentArrows()
